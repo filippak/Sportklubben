6a1fd14d08891e68414056c984f52c0bff95333d
Suphleana <ellen.andersson89@gmail.com>
2017-11-01 14:21:21 +0100

    Aside: Vertically aligned blocks and started to fix to bottom

--- js/fixed.js
+++ js/fixed.js
@@ -5,6 +5,24 @@ window.onload = function() {
 	var rect = element.getBoundingClientRect();
 	var y =  rect.height;
 
+	var elementFoot = jQuery(".l-footer");
+	elementFoot = elementFoot[0];
+	var rectFoot = elementFoot.getBoundingClientRect();
+	var topFoot = rectFoot.top;
+
+	var elementSide = jQuery('.l-sidebar');
+	elementSide = elementSide[0];
+	var rectSide = elementSide.getBoundingClientRect();
+	var botSide = rectSide.bottom;
+
+	var bodyTop = document.body.getBoundingClientRect();
+	bodyTop = bodyTop.top;
+
+	var footOffset = topFoot - bodyTop;
+	console.log(bodyTop);
+	console.log(topFoot);
+	console.log(footOffset);
+
 	var fromTop = jQuery(document).scrollTop();
 	var width = jQuery(window).width();
 
@@ -15,21 +33,24 @@ window.onload = function() {
 		var fromTop = jQuery(document).scrollTop();
 		var width = jQuery(window).width();
 
-		fixAside(y, fromTop, width);
+		fixAside(y, fromTop, width, footOffset, botSide);
 
 	});
 	
 }
 
 
-function fixAside (y, fromTop, width) {
+function fixAside (y, fromTop, width, offset, bottom) {
 	var sidebar = jQuery('.l-sidebar');
 	sidebar = sidebar[0];
 
-	if(fromTop > y && width >= 768) {
+	if(fromTop > y && width >= 768 && offset > bottom) {
 		jQuery(sidebar).addClass("l-sidebar-fix");
 
-	}else {
+	}else if(offset < bottom){
+		jQuery(sidebar).removeClass("l-sidebar-fix");
+		jQuery(sidebar).css("vertical-align", "bottom");
+	} else {
 		jQuery(sidebar).removeClass("l-sidebar-fix");
 	}
 }
