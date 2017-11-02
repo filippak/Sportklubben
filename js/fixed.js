d1618aaf39c40ce953a886a53b10661ad063837b
Ludwig Sidenmark <ludwigsidenmark@gmail.com>
2017-11-02 16:09:52 +0100

    Fixed cleanup

--- js/fixed.js
+++ js/fixed.js
@@ -19,65 +19,9 @@ window.onload = function() {
 }
 
 function checkHead (topp, headerHeight) {
-	console.log("tja");
-	console.log(topp);
-	console.log(headerHeight);
 	if(topp > headerHeight) {
 		jQuery(".l-header").addClass("isFixed");
-		console.log("hej");
 	} else {
 		jQuery(".l-header").removeClass("isFixed");
 	}
 }
-
-	//console.log(width);	
-
-// 	if(width >= 768) {
-// 		//hur mycket man scrollat
-// 		var fromTop = jQuery(document).scrollTop();
-		
-// 		var sidebar = jQuery('.sidebarInfo');
-// 		sidebar = sidebar[0];
-
-// 		//Kolla var botten av sidebar
-// 		bottomInfoActivities = jQuery(".sidebarInfo").offset().top + jQuery(".sidebarInfo").height();
-// 		//console.log(bottomInfoActivities);
-
-// 		//kolla toppen av sidebar
-// 		topInfoActivities = jQuery(".sidebarInfo").offset().top;
-// 		//console.log(topInfoActivities);
-		
-// 		checkFooter(bottomInfoActivities, topFooter);
-// 		checkHeader(topInfoActivities, headerHeight);
-
-// 		jQuery(document).scroll(function() {
-// 			//hur långt ner botten är
-// 			bottomInfoActivities = jQuery(".sidebarInfo").offset().top + jQuery(".sidebarInfo").height();
-// 			//hur långt scrollat
-// 			fromTop = jQuery(document).scrollTop();
-
-// 			checkFooter(bottomInfoActivities, topFooter)
-// 			checkHeader(fromTop, headerHeight)
-
-// 		});
-// 	}
-	
-// }
-
-// function checkFooter(sidebar, footer) {
-	
-// 	if(sidebar >= footer) {
-// 		jQuery(".sidebarInfo").addClass("sidebarBottomFix");
-// 	} else {
-// 		jQuery(".sidebarInfo").removeClass("sidebarBottomFix");
-// 	}
-// }
-
-// function checkHeader(sidebar, header) {
-// 	//
-// 	if(sidebar >= header) {
-// 		jQuery(".sidebarInfo").addClass("l-sidebar-fix");
-// 	} else {
-// 		jQuery(".sidebarInfo").removeClass("l-sidebar-fix");
-// 	}
-// }
