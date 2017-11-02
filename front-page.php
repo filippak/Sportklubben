fc74be2111449d0309d116219dc71e1caafb58d2
ViktorSucksdorff <33115221+ViktorSucksdorff@users.noreply.github.com>
2017-11-02 11:33:17 +0100

    FrontPage does not show old or recurring activities anymore.

--- front-page.php
+++ front-page.php
@@ -7,7 +7,12 @@
 	$args = array('posts_per_page=5', '&paged='.$paged, 'post_type' => 'aktiviteter');
 	$wp_query = new WP_Query($args); 
 	while ($wp_query->have_posts()) : $wp_query->the_post(); 
-?>
+	$eventTypeForThis = get_field('engangsforetelse_eller_aterkommande_aktivitet');
+	$thisEndDate = strtotime(get_field('slutdatum'));
+    $todaysDate = strtotime(date(d.m.y));
+    if($eventTypeForThis !== "aterkommande" && $thisEndDate > $todaysDate) :  ?>
+
+
 		<h2>
 			<a href="<?php the_permalink(); ?>" title="Read more">
 				<?php the_title(); ?>
@@ -15,6 +20,7 @@
 		</h2>
 		<?php the_excerpt(); ?>
 <?php 
+	endif;
 	endwhile;
 	if ($paged > 1) :
 ?>
@@ -31,5 +37,4 @@
 
 </main>
 <?php wp_reset_postdata();?>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
