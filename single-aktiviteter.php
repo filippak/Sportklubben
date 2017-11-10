04307ce965a73e3828e0bbc6357f3b2b2d5c4460
Ludwig Sidenmark <ludwigsidenmark@gmail.com>
2017-11-10 09:45:00 +0100

    close fix

--- single-aktiviteter.php
+++ single-aktiviteter.php
@@ -149,7 +149,7 @@
 				<!-- Modal content -->
 				<div class="modal-content">
 					<div class="modal-header">
-						<span class="close">&times;</span>
+						<span class="modal-close">&times;</span>
 						<h2>Modal Header</h2>
 					</div>
 					<div class="modal-body">
